7de20963a62fcca43ef5df569e5ca7fb8117c32d
Samuel Muli <samuelmuli95@gmail.com>
2023-11-24 10:38:29 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    
+    if length >= 1:
+        fib_sequence.append(0)
+    if length >= 2:
+        fib_sequence.append(1)
+    for n in range(2, length):
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+
+    print(fib_sequence)
